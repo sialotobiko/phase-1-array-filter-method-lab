5032f66f3a2bf4c98e865f8564c1c283fc1b1299
sialotobiko <sialo.tobiko@student.moringaschool.com>
2022-08-22 19:16:59 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, str1){
+    const matchingDrivers = array.filter(x => x.toLowerCase().includes(str1.toLowerCase()));
+	return matchingDrivers;
+}
+
+function fuzzyMatch(array, str2) {
+	const length = str2.length;
+	const matching = array.filter(y => y.slice(0, length) === str2);
+	return matching;
+}
+
+function matchName(array, str3) {
+	const matchingDriverNames = array.filter(z => z.name === str3);
+	return matchingDriverNames;
+}
